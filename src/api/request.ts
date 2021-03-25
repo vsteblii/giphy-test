c50beac6b0b6209522bf5c3426c7f8d59ce3aa79
Volodymyr <volodymyr.steblii@gmail.com>
2021-03-25 15:26:41 +0200

    Just comments

--- src/api/request.ts
+++ src/api/request.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'http://api.giphy.com/v1/',
     params: {
+        // todo have to be moved to env variables
         api_key: 'MPagf7eaBYKvQy94vp8AxEvTodsN0e1u',
     }
   });
