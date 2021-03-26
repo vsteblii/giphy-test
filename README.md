1f54a9e1082c519b6160889d77d42112b297b9a0
Volodymyr <volodymyr.steblii@gmail.com>
2021-03-26 16:55:32 +0200

    Update env

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Getting Started 
 * Install docker
 * Rename .env.example to .env
-    * Update 'GIPHY_API_KEY' value with your GIPHY api key
+    * Update 'REACT_APP_GIPHY_API_KEY' value with your GIPHY api key
     * docker-compose up -d
     * Wait for build 
     * Run http://localhost:3001/
--- src/api/request.ts
+++ src/api/request.ts
@@ -1,10 +1,11 @@
 
 import axios from 'axios';
+const apiKey = process.env.REACT_APP_GIPHY_API_KEY || '';
+
 const instance = axios.create({
     baseURL: 'http://api.giphy.com/v1/',
     params: {
-        // todo have to be moved to env variables
-        api_key: 'MPagf7eaBYKvQy94vp8AxEvTodsN0e1u',
+        api_key: apiKey,
     }
   });
 export default instance;
